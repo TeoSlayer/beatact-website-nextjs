0d946dfd51556655177fecca6ec4ba9e9dd22be1
Teo Calin <teodor@calin.pro>
2021-03-10 16:39:50 +0200

    Updates Errors

--- pages/Events/[id].js
+++ pages/Events/[id].js
@@ -5,30 +5,8 @@ export default function Home({event}) {
         const router = useRouter()
         const { id } = router.query
         return (
-            <h1>{event.Id}</h1>
+            <h1>{id}</h1>
         )
 }
 
 
-
-export async function getStaticProps({ params }) {
-
-    const req = await fetch("../../public/" + params.id + ".json");
-    const data = await req.json();
-
-    return {
-        props: { event : data }
-    }
-}
-
-export async function getStaticPaths() {
-    const req = await fetch("../../public/Events.json");
-    const data = await req.json();
-    const paths = data.map(event => {
-        return { params: { id : event }}
-    })
-    return {
-         paths,
-         fallback: false
-    };
-}
